235e9ea5ec16d9a0818f82a49dbb357469aae7eb
ricardo-sousa <rwmsousa@gmail.com>
2022-03-19 11:25:41 -0300

    challenge resolutions

--- src/main/java/com/trybe/acc/java/Main.java
+++ src/main/java/com/trybe/acc/java/Main.java
@@ -1,10 +1,47 @@
 package com.trybe.acc.java;
 
+import java.util.Scanner;
+
+/** Método inicial. */
 public class Main {
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    try {
+      System.out.println("\n");
+      System.out.println("Qual é o seu nome?");
+      String nome = scan.next();
+      System.out.println("\n");
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+      System.out.println("Entre com as notas das avaliações:");
+      System.out.println("Avaliação 1: ");
+      double av1 = scan.nextDouble();
+      // String av1Scan = scan.next();
+      System.out.println("\n");
 
-	}
+      System.out.println("Avaliação 2: ");
+      double av2 = scan.nextDouble();
+      // String av2Scan = scan.next();
+      System.out.println("\n");
 
+      System.out.println("Avaliação 3: ");
+      double av3 = scan.nextDouble();
+      // String av3Scan = scan.next();
+      System.out.println("\n");
+
+      System.out.println("Avaliação 4: ");
+      double av4 = scan.nextDouble();
+      // String av4Scan = scan.next();
+      System.out.println("\n");
+
+      Nota nota = new Nota();
+      double media = nota.calcularMedia(av1, av2, av3, av4);
+      System.out.println("Olá " + nome + ", sua média é " + media);
+
+      scan.close();
+
+    } catch (Exception e) {
+      System.out.println("\n");
+      System.out.println("Erro: " + e.getMessage());
+    }
+  }
 }
--- src/main/java/com/trybe/acc/java/Nota.java
+++ src/main/java/com/trybe/acc/java/Nota.java
@@ -1,5 +1,9 @@
 package com.trybe.acc.java;
 
 public class Nota {
+  /** Método inicial. */
+  public double calcularMedia(double av1, double av2, double av3, double av4) {
 
+    return (av1 + av2 + av3 + av4) / 4;
+  }
 }
